6ea99e3fb0bdbe81b6027fe693f91890e46cb30b
core23 <mail@core23.de>
2019-03-18 22:14:01 +0100

    [BUILD] Added more tests

---
+++ tests/Core23SetlistFmBundleTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core23\SetlistFmBundle\Tests;
+
+use Core23\SetlistFmBundle\Core23SetlistFmBundle;
+use Core23\SetlistFmBundle\DependencyInjection\Core23SetlistFmExtension;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+
+class Core23SetlistFmBundleTest extends TestCase
+{
+    public function testItIsInstantiable(): void
+    {
+        $bundle = new Core23SetlistFmBundle();
+
+        $this->assertInstanceOf(BundleInterface::class, $bundle);
+    }
+
+    public function testGetContainerExtension(): void
+    {
+        $bundle = new Core23SetlistFmBundle();
+
+        $this->assertInstanceOf(Core23SetlistFmExtension::class, $bundle->getContainerExtension());
+    }
+}
