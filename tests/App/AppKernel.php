3acd51c8d081e5c9849404fed84306f2b8c5ede1
core23 <mail@core23.de>
2019-12-20 16:09:11 +0100

    Add integration test

---
+++ tests/App/AppKernel.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core23\SetlistFmBundle\Tests\App;
+
+use Core23\SetlistFmBundle\Core23SetlistFmBundle;
+use Core23\SetlistFmBundle\Tests\App\Controller\TestController;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
+
+final class AppKernel extends Kernel
+{
+    use MicroKernelTrait;
+
+    public function __construct()
+    {
+        parent::__construct('test', false);
+    }
+
+    public function registerBundles()
+    {
+        yield new FrameworkBundle();
+        yield new Core23SetlistFmBundle();
+    }
+
+    public function getCacheDir(): string
+    {
+        return $this->getBaseDir().'cache';
+    }
+
+    public function getLogDir(): string
+    {
+        return $this->getBaseDir().'log';
+    }
+
+    public function getProjectDir(): string
+    {
+        return __DIR__;
+    }
+
+    protected function configureRoutes(RouteCollectionBuilder $routes): void
+    {
+        $routes->add('/test', TestController::class);
+    }
+
+    protected function configureContainer(ContainerBuilder $containerBuilder, LoaderInterface $loader): void
+    {
+        $loader->load(__DIR__.'/config/config.yaml');
+    }
+
+    private function getBaseDir(): string
+    {
+        return sys_get_temp_dir().'/app-bundle/var/';
+    }
+}
